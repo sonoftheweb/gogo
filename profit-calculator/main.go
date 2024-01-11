ead89c475bbeeb2d708b6e310b13c91ae3cbc446
Jacob Ekanem <prometheus6001@gmail.com>
2024-01-11 03:48:42 -0400

    file storage, error handling computations etc.

--- profit-calculator/main.go
+++ profit-calculator/main.go
@@ -1,17 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
-	revenue := getUserInput("Enter revenue: ")
-	totalExpenses := getUserInput("Enter total expenses: ")
-	taxRate := getUserInput("Enter tax rate: ")
+	revenue, revenueError := getUserInput("Enter revenue: ")
+	if revenueError != nil {
+		fmt.Println(revenueError)
+		return
+	}
+
+	totalExpenses, totalExpensesError := getUserInput("Enter total expenses: ")
+	if totalExpensesError != nil {
+		fmt.Println(totalExpensesError)
+		return
+	}
+
+	taxRate, taxRateError := getUserInput("Enter tax rate: ")
+	if taxRateError != nil {
+		fmt.Println(taxRateError)
+		return
+	}
 
 	earningBeforeTax, earningAfterTax, ratioEBTtoEAT := computeValues(revenue, totalExpenses, taxRate)
+
+	// store to file
+	storeToFile(revenue, totalExpenses, taxRate, earningBeforeTax, earningAfterTax, ratioEBTtoEAT)
 
 	fmt.Printf("Earnings Before Tax: %.2f\n", earningBeforeTax)
 	fmt.Printf("Earnings After Tax: %.2f\n", earningAfterTax)
 	fmt.Printf("Ratio of Earnings Before Tax to Earnings After Tax: %.2f\n", ratioEBTtoEAT)
+}
+
+func storeToFile(revenue, expenses, rate, tax, tax2, eat float64) {
+	err := os.WriteFile("profit.txt", []byte(fmt.Sprintf("Revenue: %.2f\nExpenses: %.2f\nTax Rate: %.2f\nEarnings Before Tax: %.2f\nEarnings After Tax: %.2f\nRatio of Earnings Before Tax to Earnings After Tax: %.2f\n", revenue, expenses, rate, tax, tax2, eat)), 0644)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func computeValues(revenue, totalExpenses, taxRate float64) (earningBeforeTax, earningAfterTax, ratioEBTtoEAT float64) {
@@ -22,15 +50,23 @@ func computeValues(revenue, totalExpenses, taxRate float64) (earningBeforeTax, e
 	return
 }
 
-func getUserInput(text string) float64 {
+func getUserInput(text string) (float64, error) {
 	var userInput float64
 
 	fmt.Print(text)
 	_, err := fmt.Scan(&userInput)
 	if err != nil {
-		fmt.Println("Invalid input. When prompted, please enter valid values.")
-		return 0
-	}
+		// Clear the buffer
+		var discard string
+		_, err := fmt.Scanln(&discard)
+		if err != nil {
+			return 0, err
+		}
 
-	return userInput
+		return 0, errors.New("invalid input. when prompted, please enter valid values")
+	} else if userInput <= 0 {
+		return 0, errors.New("invalid input. value must be greater than 0")
+	} else {
+		return userInput, nil
+	}
 }
